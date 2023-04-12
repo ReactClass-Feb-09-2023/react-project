cf1c72290885fa1ac8ead5d5494669e52b2cc8b0
Amr Fouad <3mrfouad@gmail.com>
2023-04-11 21:01:05 -0600

    first react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Avatar from './components/Avatar'
+import React from 'react'
+function App() {
+  return <Avatar />
+}
+
+export default App
+// JSX Rules
+// it must have one single root element per component
+// class attribute x , use className
+// for attribute x , use htmlFor
+// to render valid js expression use { }
+// to pass attribute values use { }, only exception is strings ""
---
+++ src/components/Avatar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Avatar() {
+  return (
+    <>
+      <img width="50" height="50" src="https://placehold.co/400" />
+      <span>Lorem ipsum dolor sit.</span>
+    </>
+  )
+}
+
+export default Avatar
