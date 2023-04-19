0db34d709347adcba3c75d483de63dfa9139c6e7
Amr Fouad <3mrfouad@gmail.com>
2023-04-18 18:16:22 -0600

    hide button implementation without ?: operator

--- src/components/Testimonial/index.jsx
+++ src/components/Testimonial/index.jsx
@@ -6,14 +6,14 @@ import Avatar from '../Avatar'
 
 function Testimonial({ user }) {
   const { name, profileImage, testimonial } = user
-  const [isHidden, setIsHidden] = useState(false)
+  const [containerStyles, setContainerStyles] = useState('')
 
   const handleOnClick = () => {
-    setIsHidden(true)
+    setContainerStyles('hide')
   }
 
   return (
-    <div className={`testimonial__container ${isHidden ? 'hide' : ''}`}>
+    <div className={`testimonial__container ${containerStyles}`}>
       <Avatar name={name} imageSrc={profileImage.src} />
       <p className="testimonial__text">{testimonial}</p>
       <button onClick={handleOnClick} className="testimonial__hideBtn">
